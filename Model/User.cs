9a5793b3cc708278bb97e93d76f4f526b35ad97c
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-10-30 16:11:11 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User : BaseEntity
+    {
+        protected string firstName;
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        protected string lastName;
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        protected string email;
+        public string Email { get { return email; } set { email = value; } }
+        protected string username;
+        public string Username { get { return username; } set { username = value; } }
+        protected string password;
+        public string Password { get { return password; } set { password = value; } }
+        protected string phoneNumber;
+        public string PhoneNumber { get {  return phoneNumber; } set {  phoneNumber = value; } }
+        protected bool isAdmin;
+        public bool IsAdmin { get {  return isAdmin; } set { IsAdmin = value; } }
+        protected DateTime birthday;
+        public DateTime Birthday { get {  return birthday; } set {  birthday = value; } }
+
+    }
+
+    public class UserList : List<User>
+    {
+        public UserList() { }
+        public UserList(IEnumerable<User> list) : base(list) { }
+        public UserList(IEnumerable<BaseEntity> list) : base(list.Cast<User>().ToList()) { }
+    }
+
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,48 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.ID = int.Parse(reader["id"].ToString());
+            user.FirstName = reader["firstName"].ToString();
+            user.LastName = reader["lastName"].ToString();
+            user.Email = reader["email"].ToString();
+            user.Username = reader["username"].ToString();
+            user.Password = reader["password"].ToString();
+            user.PhoneNumber = reader["phoneNumber"].ToString();
+            user.IsAdmin = bool.Parse(reader["isAdmin"].ToString());
+            user.Birthday = DateTime.Parse(reader["birthday"].ToString());
+            return user;
+        }
+
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tableUsers";
+            return new UserList(ExecuteCommand());
+        }
+
+        public User SelectById(int id) 
+        {
+            command.CommandText = "SELECT * FROM tableUsers WHERE id=" + id.ToString();
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0) return null;
+            return list[0];
+        }
+
+    }
+}
