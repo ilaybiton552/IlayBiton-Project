2f94559a0dc1ac1e968aa5b174fb3fac81d2c4ec
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-11-09 11:11:56 +0200

    Finish EventTypeDB

---
+++ ViewModel/EventTypeDB.cs
@@ -0,0 +1,39 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class EventTypeDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new EventType();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            EventType eventType = entity as EventType;
+            eventType.ID = int.Parse(reader["id"].ToString());
+            eventType.Type = reader["type"].ToString();
+            return eventType;
+        }
+
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tableEventType";
+            return new UserList(ExecuteCommand());
+        }
+
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tableEventType WHERE id=" + id.ToString();
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0) return null;
+            return list[0];
+        }
+    }
+}
