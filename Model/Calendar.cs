7228dd86969333880979f7036b33610457654ab5
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-11-09 09:30:06 +0200

    Started working on DB and Model

---
+++ Model/Calendar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Calendar : BaseEntity
+    {
+        protected string calendarName;
+        public string CalendarName 
+        { 
+            get { return calendarName; } 
+            set { calendarName = value; } 
+        }
+        protected int creator;
+        public int Creator
+        {
+            get { return creator; }
+            set { creator = value; }
+        }
+    }
+
+    public class UserCalendar : List<Calendar>
+    {
+        public UserCalendar() { }
+        public UserCalendar(IEnumerable<Calendar> list) : base(list) { }
+        public UserCalendar(IEnumerable<BaseEntity> list) : base(list.Cast<Calendar>().ToList()) { }
+    }
+
+}
---
+++ Model/Event.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Event : BaseEntity
+    {
+        enum EnumEventType
+        {
+            Event=1,
+            Task
+        }
+        protected string eventName;
+        public string EventName
+        {
+            get { return eventName; }
+            set { eventName = value; }
+        }
+
+        protected int creator;
+        public int Creator
+        {
+            get { return creator; }
+            set { creator = value; }
+        }
+        //protected EnumEventType eventType;
+        //public EnumEventType EventType
+        //{
+        //    get { return EventType; }
+        //    set { eventType = value; }
+        //}
+        protected DateTime startDate;
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+        protected DateTime dueDate;
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+        protected DateTime startTime;
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        protected DateTime endTime;
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        protected string displayColor;
+        public string DisplayColor
+        {
+            get { return displayColor; }
+            set { displayColor = value; }
+        }
+    }
+}
