02769b7a60db5506a0468e7d4409d0e487c28796
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-12-12 20:34:10 +0200

    Finish Login Window Added the wpf project to the current solution (easier to use the classes here, for example User)

---
+++ Wpf_TimeCraft_Calendar_IlayBiton/LoginWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Shapes;
+
+namespace Wpf_TimeCraft_Calendar_IlayBiton
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private User user;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            user = new User();
+            this.DataContext = user;
+        }
+
+        private void ShowPassword_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            image.Source = new BitmapImage(new Uri(@"Media\show-password.png", UriKind.Relative));
+            pbPass.Visibility = Visibility.Collapsed;
+            tbPass.Text = pbPass.Password.ToString();
+            tbPass.Visibility = Visibility.Visible;
+        }
+
+        private void HidePassword_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            image.Source = new BitmapImage(new Uri(@"Media\hide-password.png", UriKind.Relative));
+            pbPass.Focus();
+            pbPass.Visibility = Visibility.Visible;
+            tbPass.Visibility = Visibility.Collapsed;
+        }
+
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ValidPassword validPassword = new ValidPassword();
+            ValidationResult result = validPassword.Validate(((PasswordBox)sender).Password, CultureInfo.CurrentCulture);
+            if (!result.IsValid) // result not valid
+            {
+                errPass.Text = result.ErrorContent.ToString();
+                pbPass.ToolTip = result.ErrorContent;
+                pbPass.BorderThickness = new Thickness(1);
+                tbPass.BorderThickness = new Thickness(1);
+            }
+            else // result is valid
+            {
+                errPass.Text = string.Empty;
+                pbPass.ToolTip = string.Empty;
+                pbPass.BorderThickness = new Thickness(0);
+                tbPass.BorderThickness = new Thickness(0);
+                user.Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
---
+++ Wpf_TimeCraft_Calendar_IlayBiton/Validation.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Wpf_TimeCraft_Calendar_IlayBiton
+{
+    public class ValidName : ValidationRule
+    {
+        public int min { get; set; }
+        public int max { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                string name = value.ToString();
+                if (name.Length < min) // name too short
+                    return new ValidationResult(false, "Too short");
+                if (name.Length > max) // name too long
+                    return new ValidationResult(false, "Too long");
+                foreach (char c in name)
+                    if (!Char.IsLetter(c) && c != ' ')
+                        return new ValidationResult(false, "Only letters and spaces");
+                if (!Char.IsUpper(name[0]))
+                    return new ValidationResult(false, "Name must start with capital letter");
+                if (!Char.IsLetter(name[name.Length - 1]))
+                    return new ValidationResult(false, "Name can't end with space");
+                if (name.IndexOf("  ") != -1)
+                    return new ValidationResult(false, "Name must not include more than one space at a time");
+            }
+            catch (Exception ex) 
+            {
+                return new ValidationResult(false, "Error: " + ex.Message);
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+
+    public class ValidUsername : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                string username = value.ToString();
+                bool lower = false, upper = false;
+                if (username.Length < Min) // username too short
+                    return new ValidationResult(false, "Too short");
+                if (username.Length > Max) // username too long
+                    return new ValidationResult(false, "Too long");
+                foreach (char c in username)
+                    if (!Char.IsLetterOrDigit(c) && c != '_')
+                        return new ValidationResult(false, "Only letters, numbers and underscore");
+                    else
+                        if (Char.IsLower(c)) lower = true;
+                        else if (Char.IsUpper(c)) upper = true;
+                if (!Char.IsLetter(username[0]))
+                    return new ValidationResult(false, "Username must start with a letter");
+                if (!lower) return new ValidationResult(false, "Username must contain lower letter");
+                if (!upper) return new ValidationResult(false, "Username must contain upper letter");
+            }
+            catch (Exception ex)
+            {
+                return new ValidationResult(false, "Error: " + ex.Message);
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+
+    public class ValidPassword : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                string password = value.ToString();
+                bool lower = false, upper = false, number = false;
+                char[] specialChars = { '_', '-', '@', '#' };
+                if (password.Length < 6) // password too short
+                    return new ValidationResult(false, "Too short");
+                if (password.Length > 15) // password too long
+                    return new ValidationResult(false, "Too long");
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (!Char.IsLetterOrDigit(password[i]) && !specialChars.Contains(password[i]))
+                        return new ValidationResult(false, "Only letters, numbers and: '_', '-', '@', '#'");
+                    else
+                    {
+                        if (Char.IsLower(password[i]))
+                            lower = true;
+                        else if (Char.IsUpper(password[i]))
+                            upper = true;
+                        else
+                            number = true;
+                    }
+                }
+                if (!lower)
+                    return new ValidationResult(false, "Password must include at least one lower letter");
+                if (!upper)
+                    return new ValidationResult(false, "Password must include at least one capital letter");
+                if (!number)
+                    return new ValidationResult(false, "Password must include at least one number");
+                if (!Char.IsLetter(password[0]))
+                    return new ValidationResult(false, "Password must start with a letter");
+            }
+            catch (Exception ex)
+            {
+                return new ValidationResult(false, "Error: " + ex.Message);
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+
+}
